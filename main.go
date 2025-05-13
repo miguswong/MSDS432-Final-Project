04b69aafe30ca0ab5482a3258f7b0a9de1d4180a
Migus Wong <migus.wong@gmail.com>
2025-05-13 17:44:36 -0500

    fix docker connection

--- main.go
+++ main.go
@@ -8,19 +8,9 @@ import (
 	_ "github.com/lib/pq" // PostgreSQL driver
 )
 
-// type TaxiTripsRecord []struct {
-// 	Trip_id                    string `json:"trip_id"`
-// 	Trip_start_timestamp       string `json:"trip_start_timestamp"`
-// 	Trip_end_timestamp         string `json:"trip_end_timestamp"`
-// 	Pickup_centroid_latitude   string `json:"pickup_centroid_latitude"`
-// 	Pickup_centroid_longitude  string `json:"pickup_centroid_longitude"`
-// 	Dropoff_centroid_latitude  string `json:"dropoff_centroid_latitude"`
-// 	Dropoff_centroid_longitude string `json:"dropoff_centroid_longitude"`
-// }
-
 func main() {
 	// Try a simpler connection string
-	db_connection := "host=localhost port=5432 user=postgres password=root dbname=chicago_business_intelligence sslmode=disable"
+	db_connection := "host=localhost port=5433 user=postgres password=root dbname=chicago_business_intelligence sslmode=disable"
 
 	fmt.Println("Connecting to:", db_connection)
 
